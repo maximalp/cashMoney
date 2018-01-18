c41504561a19ca008440196353d59e4a7cb5edc6
samhkcho <samhkcho@gmail.com>
2018-01-18 01:37:55 -0500

    favorite button

--- client/src/components/InvoiceFavoriteFeature.js
+++ client/src/components/InvoiceFavoriteFeature.js
@@ -33,22 +33,21 @@ class InvoiceFavoriteFeature extends React.Component {
       <div>
         <div className="row">
           <div className="col s12">
-            <ul className="tabs tabs-fixed-width row">
-              <InvoiceFavoriteTabs onClick={this.handleOnClick} style={{"background":"pink"}}/>
+            {/* <ul className="tabs row tabs-fixed-width">
+              <InvoiceFavoriteTabs invoice={this.props.invoice} onClick={this.handleOnClick} style={{"background":"pink"}}/>
               <li onClick={this.change} style={{"background":"pink"}} className="tab col">button</li>
-              <li style={{"background":"pink"}} className="tab col"></li>
-              <li style={{"background":"pink"}} className="tab col disabled"></li>
-              <li style={{"background":"pink"}} className="tab col">button</li>
-              <li style={{"background":"pink"}} className="tab col">button</li>
-              <li style={{"background":"pink"}} className="tab col">button</li>
-              <li style={{"background":"pink"}} className="tab col">button</li>
 
-            </ul>
+
+            </ul> */}
+            <InvoiceFavoriteTabs className="row" invoice={this.props.invoice} onClick={this.handleOnClick} style={{"background":"pink"}}/>
+            {/* <ul>
+              <li></li>
+            </ul> */}
           </div>
-          <div id="test1" className="col s12">Test 1  {this.state.value ? 'Yo YO Yo' : ''}</div>
-          <div id="test2" className="col s12">Test 2</div>
-          <div id="test3" className="col s12">Test 3</div>
-          <div id="test4" className="col s12">Test 4</div>
+          <div id="test1" className="col m12">Test 1  {this.state.value ? 'Yo YO Yo' : ''}</div>
+          <div id="test2" className="col m12">Test 2</div>
+          <div id="test3" className="col m12">Test 3</div>
+          <div id="test4" className="col m12">Test 4</div>
         </div>
       </div>
     )
