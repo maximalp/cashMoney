5d505e92564c5fa71c7e0bb539e2b60b7a0359ff
unknown <samhkcho@gmail.com>
2018-01-22 14:19:13 -0500

    added 2/3 graphs

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -1,26 +1,190 @@
 import React from 'react';
-import BarGraph from '../BarGraph';
-import AreaGraph from '../AreaGraph';
-import PieGraph from '../PieGraph';
+// import BarGraph from '../BarGraph';
+// import AreaGraph from '../AreaGraph';
+// import PieGraph from '../PieGraph';
 import moment from 'moment';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import API from '../utils/API';
+import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ReferenceLine} from 'recharts';
+
+const data = [
+      {name: 'Total Invoices', uv: -4000}
+];
+
+
 class Dashboard extends React.Component {
   constructor(props){
     super(props);
 
     this.state = {
-      value:"CashMoney"
+      value:"CashMoney",
+      startDateBar: moment(),
+      endDateBar: moment(),
+      startDateArea: moment(),
+      endDateArea: moment(),
+      startDatePie: moment(),
+      endDatePie: moment(),
+      barGraphData:[],
+      profitBarGraphData:[],
+      maxProfitBarGraphData:0
     }
   }
 
+  componentDidMount() {
+    let query = '/api/dashboard/info';
+    API.get(query)
+    .then(res => {
+      console.log(res.data);
+      console.log(res.data.invoice)
+      console.log('PROFITS!', res.data.profit)
+      let barGraphData = res.data.invoice
+      let profitBarGraphData = res.data.profit
+      let maxProfitBarGraphData = Math.abs(profitBarGraphData[0].profit)
+      console.log('MAX', maxProfitBarGraphData)
+      this.setState({
+        barGraphData,
+        profitBarGraphData,
+        maxProfitBarGraphData
+      })
+      console.log('profitBarGraphData', profitBarGraphData[0].profit)
+    })
+    .catch(err => console.log(err) )
+  }
+
+
+  handleCalendarChangeStartBar = (date) => {
+    this.setState({
+      startDateBar:date
+    });
+  }
+
+  handleCalendarChangeEndBar = (date) => {
+    this.setState({
+      endDateBar:date
+    });
+  }
+  handleCalendarChangeStartArea = (date) => {
+    this.setState({
+      startDateArea:date
+    });
+  }
+  handleCalendarChangeEndArea = (date) => {
+    this.setState({
+      endDateArea:date
+    });
+  }
+  handleCalendarChangeStartPie = (date) => {
+    this.setState({
+      startDatePie:date
+    });
+  }
+  handleCalendarChangeEndPie = (date) => {
+    this.setState({
+      endDatePie:date
+    });
+  }
+
   render(){
+
+    // let maxHeight = Math.abs(this.state.profitBarGraphData[0].profit);
+
     return (
-      <div>
+      <div className="row">
         <h1>{this.state.value}</h1>
+        <div className="col m12">
+          <div className='row'>
+            <div className="col m12">
+              {/* Invoices */}
+              <BarChart width={500} height={500} data={this.state.barGraphData}
+                    margin={{top: 20, right: 30, left: 20, bottom: 5}}>
+               <XAxis dataKey="name"/>
+               <YAxis/>
+               <CartesianGrid strokeDasharray="3 3"/>
+               <Tooltip/>
+               <ReferenceLine y={0} stroke='#000' />
+               <Legend />
+               <Bar dataKey="sent" stackId="a" fill="#8884d8" />
+               <Bar dataKey='paid' stackId="a" fill="#82ca9d" />
+              </BarChart>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col m6">
+              <DatePicker
+                selected={this.state.startDateBar}
+                onChange={this.handleCalendarChangeStartBar}
+              />
+              {/* Profits */}
+              <BarChart width={500} height={500} data={this.state.profitBarGraphData}
+                    margin={{top: 20, right: 30, left: 20, bottom: 5}}>
+               <XAxis dataKey="name"/>
+               <YAxis domain={['auto', this.state.maxProfitBarGraphData ]} />
+               <CartesianGrid strokeDasharray="3 3"/>
+               <ReferenceLine y={0} stroke='#000' />
+               <Tooltip/>
+               <Legend />
+               <Bar dataKey="profit" stackId="a" fill="#8884d8" />
+              </BarChart>
+            </div>
+            <div className="col m6">
+              <DatePicker
+                selected={this.state.endDateBar}
+                onChange={this.handleCalendarChangeEndBar}
+              />
+            </div>
+          </div>
+
+
+        </div>
+        <div className="col m12">
+          {/* <AreaGraph /> */}
+          <div className="row">
+            <div className="col m6">
+              <DatePicker
+                selected={this.state.startDateArea}
+                onChange={this.handleCalendarChangeStartArea}
+              />
+            </div>
+            <div className="col m6">
+              <DatePicker
+                selected={this.state.endDateArea}
+                onChange={this.handleCalendarChangeEndArea}
+              />
+            </div>
+          </div>
+
+
+        </div>
+        <div className="col m12">
+          {/* <PieGraph /> */}
+          <div className="row">
+            <div className="col m6">
+              <DatePicker
+                selected={this.state.startDatePie}
+                onChange={this.handleCalendarChangeStartPie}
+              />
+            </div>
+            <div className="col m6">
+              <DatePicker
+                selected={this.state.endDatePie}
+                onChange={this.handleCalendarChangeEndPie}
+              />
+            </div>
+          </div>
+
+
+        </div>
+
+
         <div>
-          <BarGraph />
-          <AreaGraph />
-          <PieGraph />
-         
+
+
+
+
+
+
         </div>
       </div>
     )
--- routes/DashboardAPI.js
+++ routes/DashboardAPI.js
@@ -1,29 +1,53 @@
-// var Dashboard = require("../models/dashboard");
-
-// Includes Routing and Controller for now...
+var Invoices = require("../models/invoice");
+var Expenses = require("../models/expense");
 
 module.exports = function(app) {
-  app.get("/about", function(req, res) {
+  app.get("/api/dashboard/info", function(req, res) {
+    Invoices.find({}).select('status lineTotal')
+      .then(dbModelInvoices => {
+
+        let sent = dbModelInvoices.filter((invoice) => {
+          return invoice.status === 'Sent'
+        })
+        let sentTotal = sent.reduce((sum, amount) => {
+          return sum + amount.lineTotal;
+        }, 0)
+
+        let paid = dbModelInvoices.filter((invoice) => {
+          return invoice.status === 'Paid'
+        })
+        let paidTotal = paid.reduce((sum, amount) => {
+          return sum + amount.lineTotal;
+        }, 0)
+
+
+
+        let invoiceAmounts = [{name: 'Total Invoices', sent:sentTotal, paid:paidTotal}]
+
+        Expenses.find({}).select('total category')
+          .then(dbModelExpense => {
+
+            let expenseTotal = dbModelExpense.reduce((sum, amount) => {
+              return sum + amount.total
+            }, 0)
+
+            let profitTotal = paidTotal - expenseTotal
+
+            let profit = [{name: 'Profit', profit:profitTotal}]
+
+
+            res.json({invoice:invoiceAmounts, expense: dbModelExpense, profit:profit})
+          })
+
+      })
+      .catch(err => {
+        res.status(422).json(err);
+      })
+
     // Dashboard.find({})
     // .then(dbModel => res.json(dbModel))
     // .catch(err => res.status(422).json(err));
     // console.log("Hi there test")
   })
 
-  app.get("/make", function(req,res) {
-    var data = {
-      title: "razer",
-      author:"razerauthor",
-      synopsis: "hi there"
-    };
-
-    // Dashboard.create(data)
-    //   .then(function(doc) {
-    //     console.log(doc);
-    //     res.json(doc);
-    //   })
-    //   .catch(function(err) {
-    //     console.log(err.message);
-    //   })
-  })
 }
